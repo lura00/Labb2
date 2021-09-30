3e27cfbb48b068626ecc1ee7b307b90d75cf6e15
kjebo <kjellbovin@hotmail.com>
2021-09-30 11:40:02 +0200

    added code

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 def body_surface_area(weight, height):
     print("Calculates Body surface area (BSA) from your height and weight:")
-    height = input ("Enter your height (cm): ")
-    weight = input ("Enter your weight (kg): ")
     height = eval(height)
     weight = eval(weight)
     bsa = 0.007184 * (weight**0.425)  * (height**0.725)
